30ce7f306cfd1086137bb707704650757ee8e828
Sille Van Landschoot <info@sillevl.be>
2018-12-05 10:51:33 +0100

    adding MQTT

--- js/app.js
+++ js/app.js
@@ -1,23 +1,39 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-    var app = new Vue({
-        el: '#app',
-        data: {
-          amount: 5
-        },
-        methods: {
-            buttonClicked: function() {
-                this.amount += 1
-            },
-            calculateColor: function() {
-                let color = 'red'
-                if(this.amount == 10) {
-                    color = 'green'
-                }
-                return {
-                    backgroundColor: color
-                }
-            }
+document.addEventListener("DOMContentLoaded", function() {
+  Vue.use(VueMqtt.default, `mqtt://172.16.72.1:9001`)
+  var app = new Vue({
+    el: "#app",
+    data: {
+      amount: 5,
+      value: '-'
+    },
+    methods: {
+      buttonClicked: function() {
+        this.amount += 1;
+      },
+      calculateColor: function() {
+        let color = "red";
+        if (this.amount == 10) {
+          color = "green";
         }
-      })
-})
+        return {
+          backgroundColor: color
+        };
+      }
+    },
+    mounted() {
+      console.log("subscribe to mqtt")
+      this.$mqtt.subscribe('plc1200_50_pn1/demo')
+      this.$mqtt.subscribe('plc1200_50_pn1/temperatuur')
+    },
+    mqtt: {
+      'plc1200_50_pn1/demo': function (message) {
+        console.log(message)
+        this.value = new TextDecoder('utf-8').decode(message)
+      },
+      'plc1200_50_pn1/temperatuur': function (message) {
+        console.log(message)
+        this.value = new TextDecoder('utf-8').decode(message)
+      }
+    }
+  });
+});
